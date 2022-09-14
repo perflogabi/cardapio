98945760b155adad03ea4fa2d9a3b25d47babb6c
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-13 22:12:06 -0300

    quarto commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
