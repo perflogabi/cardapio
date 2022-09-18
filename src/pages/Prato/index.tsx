803f6267ecfbaba51cbe7e63ba16eed228040365
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-18 14:39:21 -0300

    nono commit

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Ptao.module.scss';
+import { useParams } from 'react-router-dom';
+
+export default function Prato() {
+  return (
+    <div>
+      Prato</div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from 'components/PaginaPadrao/index';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ export default function AppRouter() {
           <Route path="/cardapio" element={<Cardapio />} />
           <Route path='sobre' element={<Sobre />} />
           <Route path='*' element={<NotFound />} />
+          <Route path='prato/:id' element={<Prato />} />
         </Routes>
         <Footer />
       </Router>
